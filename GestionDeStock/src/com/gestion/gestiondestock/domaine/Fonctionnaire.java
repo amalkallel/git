2bd293cc7cc70ad110f774270fbb64ae0aae532e
Amal Kallel <amal.kallel@gmail.com>
2017-03-09 20:55:27 +0100

    Create new web project

---
+++ GestionDeStock/src/com/gestion/gestiondestock/domaine/Fonctionnaire.java
@@ -0,0 +1,11 @@
+package com.gestion.gestiondestock.domaine;
+
+public class Fonctionnaire {
+	protected int id_fonctionnaire;
+	protected String nom;
+	protected String prenom;
+	protected String tel;
+	protected String adresse;
+	protected String region;
+
+}
---
+++ GestionDeStock/src/com/gestion/gestiondestock/domaine/commande.java
@@ -0,0 +1,8 @@
+package com.gestion.gestiondestock.domaine;
+
+public class commande {
+
+	private int num_commande;
+	private String date_commande;
+	private int quantite;
+}
